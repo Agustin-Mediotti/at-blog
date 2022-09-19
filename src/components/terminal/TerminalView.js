cee9c7acbe0b77c720d9909f27aaef5931135645
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-09-19 03:51:27 -0300

    Added prompt styles

--- src/components/terminal/TerminalView.js
+++ src/components/terminal/TerminalView.js
@@ -7,6 +7,76 @@ function TerminalView(props) {
 
   let lastItem = currentDirArr[currentDirArr.length - 1];
 
+    const themes = {
+  "my-custom-theme": {
+    themeBGColor: "#201f1e",
+    themeToolbarColor: "#DBDBDB",
+    themeColor: "#FFFEFC",
+    themePromptColor: "#FFFEFC",
+    promptColor: "#15a24d",
+    errorColor: "#b52606",
+    folderColor: "#96c0c9",
+    branchColor: "#bc69b3",
+    versionColor: "#15a24d",
+    linkColor: "#e5e5e5"
+  }
+  // etc etc
+  };
+
+  const error = {
+  color: themes["my-custom-theme"].errorColor,
+  fontWeight: "bold"
+  };
+
+  const folder = {
+    color: themes["my-custom-theme"].folderColor,
+    fontWeight: "bold"
+  }
+
+  const branch = {
+    color: themes["my-custom-theme"].branchColor,
+    fontWeight: "bold"
+  }
+
+  const version = {
+    color: themes["my-custom-theme"].versionColor,
+    fontWeight: "bold"
+  }
+
+  const promptColor = {
+    color: themes["my-custom-theme"].promptColor,
+    fontWeight: "bold"
+  }
+
+    const prompt = (
+    <>
+      <br />
+      <span style={promptColor}>
+         ❯
+      </span>
+    </>
+  )
+
+  let promptInProject = (
+    <>
+      <br />
+      <span style={folder}>
+        {currentDirArr.join("/") + "/"}
+      </span>
+      <span> on </span>
+      <span style={branch}>
+         main
+      </span>
+      <span> via </span>
+      <span style={version}>
+         17.6.0
+      </span>
+      <br />
+      <span style={promptColor}>
+         ❯
+      </span>
+    </>
+  )
 
   const commands = {
     whoami: "net_creature",
@@ -32,48 +102,17 @@ function TerminalView(props) {
     </span>
   );
 
-  const prompt = (
-    <>
-      <span>
-        <br />
-        {" "}
-        {currentDirArr.join("/") + "/"} in nodejs-project on  trunk via  17.6.0
-        <br />
-      </span>
-
-      <span>
-        {" "}
-         ❯
-      </span>
-
-    </>
-  )
-
-    const errMessage = (
-    <span>
-      {" "}
-      bash: command not found
-    </span>
-  )
-
   return (
     <>
       <div className="termcontainer">
         <ReactTerminal
           commands={commands}
-            themes={{
-              "my-custom-theme": {
-                themeBGColor: "#201f1e",
-                themeToolbarColor: "#DBDBDB",
-                themeColor: "#FFFEFC",
-                themePromptColor: "#15a24d"
-              }
-            }}
+          themes={themes}
           welcomeMessage={welcomeMsg}
-          prompt={prompt}
+          prompt={promptInProject}
           theme="my-custom-theme"
           showControlBar={false}
-          errorMessage={errMessage}
+          errorMessage={<span style={error}>bash: command not found</span>}
         />
       </div>
     </>
